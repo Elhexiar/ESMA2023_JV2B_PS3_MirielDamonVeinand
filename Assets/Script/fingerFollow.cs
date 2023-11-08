22ef3afdb392159a6fd341d20861b9b8a1e62144
Elhexiar <118454205+Elhexiar@users.noreply.github.com>
2023-11-08 11:53:52 +0100

    dqzfqzfqz

--- Assets/Script/fingerFollow.cs
+++ Assets/Script/fingerFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -5,8 +6,13 @@ using UnityEngine;
 
 public class MonScript : MonoBehaviour
 {
-
+    [SerializeField]
+    private Vector3 tempPosition;
+    [SerializeField]
+    private Vector3 delta;
     public Vector3 position;
+    public Quaternion newRotation;
+    public float normalizedRot;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,17 +29,24 @@ public class MonScript : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Camera mainCamera = Camera.main;
-            Vector3 tempPosition = new Vector3(Input.touches[0].position.x, Input.touches[0].position.y, mainCamera.nearClipPlane + 10);
+            tempPosition = new Vector3(Input.touches[0].position.x, Input.touches[0].position.y, mainCamera.nearClipPlane + 10);
+            delta = Input.touches[0].deltaPosition;
+            delta.Normalize();
+            
+            normalizedRot = 180+Mathf.Atan2(delta.y, delta.x) * 180 / 3.14f;
+
+            transform.rotation = Quaternion.Euler(0,0, normalizedRot);
+            
+            
+            //transform.rotation.SetEulerRotation(0, 0, normalizedRot);
+            //transform.rotation.Set(0, 0, normalizedRot);
+
             transform.position = mainCamera.ScreenToWorldPoint(tempPosition);
         }
 
     }
 
 
-    private void OnMouseDown()
-    {
-       
-    }
 
     
 
